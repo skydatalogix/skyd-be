bf8d9073a9bacc57dc69f79cbe36518fa1e385fc
Uday Sai Tyada <udaysai2551998@gmail.com>
2024-11-16 20:28:39 +0530

    db url

--- app/database.py
+++ app/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://root:password@3.106.53.2:5432/postgres"
+SQLALCHEMY_DATABASE_URL = "postgresql://root:password@3.106.237.16:5432/postgres"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
