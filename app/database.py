cf1351d8472a41d7ff5ad05774a366851b8f8de6
Uday Sai Tyada <udaysai2551998@gmail.com>
2024-11-16 19:27:12 +0530

    Updating the DB url

--- app/database.py
+++ app/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://root:password@3.27.63.161:5432/postgres"
+SQLALCHEMY_DATABASE_URL = "postgresql://root:password@3.106.53.2:5432/postgres"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
